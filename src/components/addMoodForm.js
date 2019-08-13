30be51d85c1e57cdcf4364047b782560e6495ac5
Stanley Huang <huang.stanley050@gmail.com>
2019-08-13 12:24:55 +1000

    able to add local state in addMood form component

--- src/components/addMoodForm.js
+++ src/components/addMoodForm.js
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
+const useMood = () => {
+  const [moodForm, setValue] = useState({
+    mood: "",
+    description: ""
+  });
+  const handleChange = e => {
+    setValue({
+      ...moodForm,
+      [e.target.name]: e.target.value
+    });
+  };
+  const resetFields = fieldName => {
+    setValue({
+      ...moodForm,
+      mood: "",
+      description: ""
+    });
+  };
+
+  return [moodForm, handleChange, resetFields];
+};
+
 const AddMoodForm = props => {
+  const [moodForm, handleChange, resetFields] = useMood();
+  //console.log(moodForm);
   return (
     <section style={{ width: "500px", margin: "2rem auto" }}>
       <Form>
         <FormGroup>
           <Label for="moods description">Description</Label>
           <Input
+            onChange={handleChange}
             type="text"
             name="description"
             id="description"
@@ -16,36 +41,74 @@ const AddMoodForm = props => {
         </FormGroup>
         <FormGroup tag="fieldset">
           <legend>How you feeling</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Happy
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Sad
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Scared
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Angry
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Surprise
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Disgust
-            </Label>
-          </FormGroup>
+          <div className="d-flex justify-content-around">
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="happy"
+                />{" "}
+                Happy
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="sad"
+                />{" "}
+                Sad
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="scared"
+                />{" "}
+                Scared
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="angry"
+                />{" "}
+                Angry
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="surprise"
+                />{" "}
+                Surprise
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label className="btn btn-info" check>
+                <Input
+                  onChange={handleChange}
+                  type="radio"
+                  name="mood"
+                  value="disgust"
+                />{" "}
+                Disgust
+              </Label>
+            </FormGroup>
+          </div>
         </FormGroup>
       </Form>
     </section>
